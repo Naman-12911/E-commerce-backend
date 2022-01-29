83679ff5349f1913036f9011445b2e71abdce644
Naman Garg <66183578+Naman-12911@users.noreply.github.com>
2022-01-29 19:41:20 +0530

    Update settings.py

--- e_commerce/settings.py
+++ e_commerce/settings.py
@@ -187,8 +187,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = "ancoder01@gmail.com"
-EMAIL_HOST_PASSWORD = "fevtjokuazoahsfw"
+
 
 # allow localhost 3000 to acced apis
 CORS_ALLOWED_ORIGINS = [
